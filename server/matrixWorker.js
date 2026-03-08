ffa91483a582368af3bfc1ac772b0a0d07c73461
DragonKael <58719353+DragonKael@users.noreply.github.com>
2026-03-08 13:09:56 -0500

    Refactor variable declarations in multiplyMatrices

--- server/matrixWorker.js
+++ server/matrixWorker.js
@@ -1,8 +1,8 @@
 const { parentPort, workerData } = require('worker_threads');
 
 function multiplyMatrices(A, B) {
-    let N = A.length;
-    let result = Array.from({ length: N }, () => Array(N).fill(0));
+    const N = A.length;
+    const result = Array.from({ length: N }, () => Array(N).fill(0));
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < N; j++) {
             for (let k = 0; k < N; k++) {
